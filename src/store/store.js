fdcc76c356227f5543c205380ada8c84b15ea21b
ErosCode <61516665+ErosCode@users.noreply.github.com>
2020-09-01 12:39:16 +0200

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -3,14 +3,14 @@ import { createStore, compose, applyMiddleware } from 'redux';
 
 // == Import : local
 import rootReducer from '../reducers';
-import logMiddleware from '../middlewares/logMiddleware';
+import middlewares from '../middlewares';
 
 // == Enhancers
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const enhancers = composeEnhancers(
   applyMiddleware(
-    logMiddleware,
+    ...middlewares,
     // secondMiddleware,
   ),
 );
